11695ac65cff0e9475562389fedb970d6fd3eff9
Morbotu <romboutlambert@gmail.com>
2021-09-28 15:07:56 +0200

    Remove comments

--- messure_distance.py
+++ messure_distance.py
@@ -7,17 +7,9 @@ import numpy as np
 
 class DepthCamera:
     def __init__(self):
-        # load mono depth network
         self.net = jetson.inference.depthNet()
-
-        # depthNet re-uses the same memory for the depth field,
-        # so you only need to do this once (not every frame)
         self.depth_field = self.net.GetDepthField()
-
-        # cudaToNumpy() will map the depth field cudaImage to numpy
-        # this mapping is persistent, so you only need to do it once
         self.depth_numpy = jetson.utils.cudaToNumpy(self.depth_field)
-
         self.camera = jetson.utils.videoSource("csi://0")
 
     def distance_reached(self, threshold_distance=2.0):
@@ -30,6 +22,6 @@ class DepthCamera:
 if __name__ == "__main__":
     depth_camera = DepthCamera()
     while True:
-        distance = depth_camera.distance_reached(1.5)
-        if distance > 50000:
-            print(f"To close {distance}")
+        pixel_closer = depth_camera.distance_reached(1.5)
+        if pixel_closer > 50000:
+            print(f"To close {pixel_closer}")
