23791c55bee47f4c0aa7964a1e65083b6af12c75
Morbotu <romboutlambert@gmail.com>
2021-09-29 19:55:27 +0200

    Change ai_kit to parent

--- src/ai_toolkit/ai_kit.py
+++ src/ai_toolkit/ai_kit.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
-from messure_distance import DepthCamera
-from detect_person import DetectionCamera
 import jetson.utils
-import numpy as np
 
 
-class AiToolkit(DepthCamera, DetectionCamera):
-    pass
-
-if __name__ == "__main__":
-    camera = jetson.utils.videoSource("csi://0")
-    ai_toolkit = AiToolkit()
-    while True:
-        img = camera.Capture()
-        detections, shape = ai_toolkit.get_detection_array(img)
-        print([person.Center for person in detections], shape)
+class AiToolkit:
+    def __init__(self, camera):
+        self.net = None
+        if camera:
+            self.camera = jetson.utils.videoSource(camera)
